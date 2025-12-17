70ddf3a311448f56fc77d53c95d56b4c3584fbc9
Victor Lacerda <victor.g03@aluno.ifsc.edu.br>
2025-12-17 17:30:12 -0300

    add: diagramas de atividades

---
+++ ENGENHARIA_SOFTWARE/UnitTests/src/main/java/br/edu/ifsc/sj/ads/es1/Calculator.java
@@ -0,0 +1,12 @@
+package br.edu.ifsc.sj.ads.es1;
+
+public class Calculadora {
+
+    static double add(double a, double b) {
+
+    }
+
+    static double multiply(double a, double b) {
+        
+    }
+}
---
+++ ENGENHARIA_SOFTWARE/UnitTests/src/test/java/br/edu/ifsc/sj/ads/es1/CalculatorTest.java
@@ -0,0 +1,16 @@
+package br.edu.ifsc.sj.ads.es1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void multiply() {
+    }
+}
